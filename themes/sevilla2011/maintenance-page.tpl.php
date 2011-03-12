efb82293e3eb1eae41bfdcfa0b92703e80cce229
Christian López Espínola <penyaskito@computer.org>
2011-03-12 20:27:21 +0100

    Added sevilla2011 theme

---
+++ themes/sevilla2011/maintenance-page.tpl.php
@@ -0,0 +1,22 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+   
+      <title>MILKTEETH</title>
+   </head><body>
+      <center>
+      <table width="600">
+	 <tbody><tr>
+            <td>
+		<br><p><br></p><p><br></p><p>   <br></p><p><br></p><p>    
+            </p></td>
+         </tr>
+         <tr>
+            <td>
+               <a href="mailto:info@milkteeth.es">
+                  <img src="logoMilkteeth.png" alt="INFORMATE: info@milkteeth.es"  align="CENTER" border="0">
+               </a>
+            </td>
+         </tr>
+      </tbody></table>
+      </center>
+   </body></html>
